01d2cd5901a49b53ceb4ffec2f9ebdd8dbeae310
abdulkadir18apr <114944605+abdulkadir18apr@users.noreply.github.com>
2023-05-14 21:11:21 +0530

    --initial setup
    create entry points
    connection with db

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const env = require('dotenv').config({ path: __dirname + '/.env' })
+const connectToMongo=require("./db");
+
+ connectToMongo();
+
+
+
+
+
+var cors = require('cors')
+
+const app = express();
+
+const port = 8000
+app.use(cors())
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
+
+//middleware
+
+
+app.use(express.json())
+
+//Routes
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello Abdul!')
+})
+
+app.listen(port, () => {
+  console.log(`Todo Listneing on port ${port}`)
+})
